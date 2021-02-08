637d66e26f72fb97d444a48a3c46ead3588ad0cd
Simião Guevan Jr <guevanjr@users.noreply.github.com>
2021-02-08 10:43:49 +0200

    More changes

--- appManager.js
+++ appManager.js
@@ -3,44 +3,67 @@ const app = express();
 const bodyParser = require('body-parser');
 const redis = require('redis');
 const client = redis.createClient();
+const cors = require('cors');
+const morgan = require ('morgan');
 
-app.use(bodyParser.raw({ type: function() {
-    return true;
-},
-limit: '5mb'
-}));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
 
-//const smsRoutes = require('./routes/sms.routes.js');
+const dashboardRoutes = require('./routes/dashboard.routes.js');
 
 // using as middleware
-//app.use('/sms', smsRoutes);
+app.use('/dashboard', smsRoutes);
 
 // Root handler
 app.get('/', function (req, res) {
+    /*
     client.send_command('select', [10], redis.print);
-var r = {};
+    var r = {};
 
-client.keys('*', function(err, keys) {
-  async.each(keys, function(key, callback) {
-    client.get(key, function(err, value) {
-      r[key] = value;
-      callback(err);
+    client.keys('*', function(err, keys) {
+        async.each(keys, function(key, callback) {
+        client.get(key, function(err, value) {
+        r[key] = value;
+        callback(err);
+        });
+    }, function() {
+            // when callback is finished
+            console.log(JSON.stringify(r));
+            client.quit();
+        });
     });
-  }, function() {
-    // when callback is finished
-    console.log(JSON.stringify(r));
-    client.quit();
-  });
-});
+
   res.send('Bem-vindo ao Dashboard da AdeM!<br /><a href="https://github.com/guevanjr/appManager/#readme">Git README</a><br />' + JSON.stringify(r));
+  */
+    res.json({
+        message: 'Dashboard da AdeM'
+    });
 })
 
-app.post('/', function (req, res) {
-    res.send('');
+app.post('/dashboard', function (req, res) {
+    client.send_command('select', [10], redis.print);
+    var r = {};
+
+    client.keys('*', function(err, keys) {
+        async.each(keys, function(key, callback) {
+        client.get(key, function(err, value) {
+        r[key] = value;
+        callback(err);
+        });
+    }, function() {
+            // when callback is finished
+            console.log(JSON.stringify(r));
+            res.json(r)
+            client.quit();
+        });
+    });
+
+
 })
 
 // Launch the server and listen
-var port = 6000;
+const port = process.env.PORT || 6000;
 app.listen(port, function () {
     console.log('Listening to Dashboard App on port ' + port);
 
